327a34c1b42b5fdc445460f81eab5c78ce123278
Alan Krajina <alankrajina88@gmail.com>
2019-05-02 15:32:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,12 @@
-def turn_count
+def turn_count(board)
+  counter=0
+  board.each do |turn|
+   if turn == "X" || turn == "O"
+     
+    counter +=1
+  end
+  return 
 end
 
+def current_player
+end
